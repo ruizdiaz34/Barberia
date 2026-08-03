b3dc97e6b8fa352ae7ef236033a0a888e47c8fe4
Leonardo <razonesdelavida@gmail.com>
2026-08-03 08:40:26 -0300

    Cambios

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -1,8 +1,8 @@
 export default function Contact() {
     return (
         <>
-        <div className="bg-red-500 text-white p-8 flex flex-col items-center justify-center min-h-screen">
-            <h2 className="text-2xl font-bold mb-4">Estilo que habla por vos</h2>
+        <div className="bg-[url('/barberia.png')] bg-cover bg-center min-h-screen flex flex-col items-center justify-center text-white">
+            <h2 className="text-2xl font-bold mb-4 text-white">Estilo que habla por vos</h2>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 Reserva tu turno
             </button>
